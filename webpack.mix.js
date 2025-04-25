35bd1ade870c60e12c63e0a2ffdb66a647892436
CarlosAbrahamMR <carlos.abraham2000@gmail.com>
2025-04-25 16:14:42 -0600

    5ewo

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,11 @@
 const mix = require('laravel-mix');
 
 
+mix.autoload({
+    jquery: ['$', 'window.jQuery', 'jQuery'],
+});
 
+mix.setPublicPath('public');
 
 mix.js('resources/js/app.js', 'public/js')
    .vue()
