8d572007d7c747cbc26ce9414873a09986301565
CarlosAbrahamMR <carlos.abraham2000@gmail.com>
2025-04-28 13:48:26 -0600

    dsaf

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -13,7 +13,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- jQuery -->
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-<script src="/js/plugins/jquery.breakpoints.min.js"></script>
+<script src="{{ asset('js/plugins/jquery.breakpoints.min.js') }}"></script>
+
 
     <!-- Primero JQuery y plugins -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
