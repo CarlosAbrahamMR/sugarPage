b3c71745e0483c889e78499c3da2bd94efa2eed6
CarlosAbrahamMR <carlos.abraham2000@gmail.com>
2025-04-30 17:14:56 -0600

    ashfas

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ mix.setPublicPath('public');
 mix.scripts([
     'public/js/vendor/jquery-3.5.1.min.js',
     'public/js/vendor/jquery.cookie.min.js',
-    'public/js/plugins/jquery-breakpoints.min.js',
+    'public/js/plugins/jquery.breakpoints.min.js',
     'public/js/vendor/moment.min.js',
     'public/js/vendor/daterangepicker.min.js',
     'public/js/vendor/bootstrap.bundle.min.js',
